d6da1f4925c13b8f2ed6205bf691c09e3717a94e
Colin Chin Choy <cchinchoy@gmail.com>
2022-10-24 21:43:41 -0400

    Options Added issues with Element capture

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_experimental_option('detach', True)
 
-chrome_selenium_driver_path = 'C:\bin\chromedriver.exe'
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-PROMISED_DOWN = 300
-PROMISED_UP = 20
+driver.get("https://speedtest.net/")
 
-START_TEST = "js-start-test test-mode-multi"
+start_btn = WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,".start-button [href]")))
+start_btn.click()
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
+data_dwn = WebDriverWait(driver,60).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,".result-data-large [class]")))
+for item in data_dwn:
+    print(item.text)
